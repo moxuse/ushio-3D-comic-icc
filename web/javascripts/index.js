aa3b32977bb65627b2a4da319aac2775c14af9ea
moxuse <moxuse@gmail.com>
2022-01-06 15:50:55 +0900

    instruction

--- web/javascripts/index.js
+++ web/javascripts/index.js
@@ -2,6 +2,7 @@ var container, controls;
 var camera, scene, renderer, light;
 var from, to, wheelPosition;
 var loading = document.getElementById('loading');
+var instruction = document.getElementById('instruction');
 var md = new MobileDetect(window.navigator.userAgent);
 var isMobile = md.mobile();
 var timerId;
@@ -85,7 +86,15 @@ function init(config) {
     loading.classList.add('loaded');
     setTimeout(() => {
       loading.classList.add('hidden');
-    }, 2000)
+    }, 2000);
+
+    setTimeout(() => {
+      instruction.classList.add('loaded');
+    }, 5000);
+
+    setTimeout(() => {
+      instruction.classList.add('hidden');
+    }, 7000);
   });
   // var loader = new THREE.FBXLoader();
   // loader.load('../model/ComicScene.fbx', function (object) {
